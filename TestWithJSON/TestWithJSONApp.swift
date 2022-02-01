0d81823fb0caa700089ed0c3cbd595724f0c023d
Alessandro Dovere <a.dovere@quadronica.com>
2022-02-01 12:36:04 +0100

    Initial Commit

---
+++ TestWithJSON/TestWithJSONApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestWithJSONApp.swift
+//  TestWithJSON
+//
+//  Created by Alessandro Dovere on 01/02/22.
+//
+
+import SwiftUI
+
+@main
+struct TestWithJSONApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
