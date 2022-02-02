134f8ce9713e6d1acbdeb0bf334bf542a80c79ac
Alessandro Dovere <a.dovere@quadronica.com>
2022-02-02 17:47:21 +0100

    added navigation link to the photos

--- TestWithJSON/Views/Photos/PhotosView.swift
+++ TestWithJSON/Views/Photos/PhotosView.swift
@@ -10,21 +10,37 @@ import SwiftUI
 struct PhotosView: View {
     @StateObject var viewModel = PhotosViewModel()
     var columnGrid: [GridItem] = [GridItem(.flexible()),GridItem(.flexible()),GridItem(.flexible())]
+
     var body: some View {
-        ScrollView {
-            LazyVGrid(columns: columnGrid) {
-                    ForEach(viewModel.photos) { photo in
-                        AsyncImage(url: URL(string: photo.url)) { image in
-                            VStack {
-                                image.resizable()
-                                    .frame(width: (UIScreen.main.bounds.width/3) - 1, height: (UIScreen.main.bounds.width/3) - 1)
-                            }
-                        } placeholder: {
-                            VStack {
-                                Image(systemName: "photo")
-                            }
-                        }
+        NavigationView {
+            ScrollView {
+                LazyVGrid(columns: columnGrid) {
+                    asyncImages
+                }
+            }
+            .navigationTitle(Text("Photos"))
+        }
+    }
+
+    var asyncImages: some View {
+        ForEach(viewModel.photos) { photo in
+            AsyncImage(url: URL(string: photo.url)) { image in
+                NavigationLink(destination: {
+                    ZStack {
+                        AsyncImage(url: URL(string: photo.url))
+                        Text(photo.title)
                     }
+                }, label: {
+                    VStack {
+                        image
+                            .resizable()
+                            .frame(width: (UIScreen.main.bounds.width/3) - 1,
+                                   height: (UIScreen.main.bounds.width/3) - 1)
+                    }                })
+            } placeholder: {
+                VStack {
+                    Image(systemName: "photo")
+                }
             }
         }
     }
